5c1d846a25fae19ff2aedbd4d36fd6904293c039
matteomureddu <matteomureddu14@gmail.com>
2023-04-01 15:51:30 +0200

    prima bozza thread + input in main

--- Es3_thread_mureddu.java
+++ Es3_thread_mureddu.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 /**
  * Es3_thread_mureddu
  */
 public class Es3_thread_mureddu {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);  
+        System.out.println("Iserisci valore n: ");
+
+        String n = scan.nextLine();
+
+        System.out.println("Inserisci valore k: ");
+
+        String k = scan.nextLine();
+
         
     }
 
     public static class Fattoriale extends Thread{
-        
+        private int val;
+
+        public int getVal(){
+            return this.val;
+        }
+
+        @Override
+        public int run() {
+            int fattoriale = val;
+            for(int i=val-1; i>0; i--) {
+                fattoriale *= i;
+            }
+            return fattoriale;
+        }
     }
     
 }
