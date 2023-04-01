6fcfc167329bf87bd7b1f6f1465eade740c06c87
matteomureddu <matteomureddu14@gmail.com>
2023-04-01 15:39:22 +0200

    primo commit

---
+++ Es3_thread_mureddu.java
@@ -0,0 +1,14 @@
+/**
+ * Es3_thread_mureddu
+ */
+public class Es3_thread_mureddu {
+    public static void main(String[] args) {
+        
+    }
+
+    public static class Fattoriale extends Thread{
+        
+    }
+    
+}
+
